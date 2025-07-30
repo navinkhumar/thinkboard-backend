3c49ee612f3f3b677fa11df6c4ee970995d8a5c9
Navin khumar <navinkhumar2005@gmail.com>
2025-07-30 11:17:20 +0530

    enabled cors

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 // middleware
   app.use(
     cors({
-      origin: ["http://localhost:5173"]
+      origin: ["https://thinkboard-seven.vercel.app/"]
     })
   );
 
